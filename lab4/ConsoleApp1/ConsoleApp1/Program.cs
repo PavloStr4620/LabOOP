72a31cc658bcc24e67ffd418c2994960d238038c
Pavlo <112697824+PavloStr4620@users.noreply.github.com>
2024-11-28 22:22:39 +0200

    Update Program.cs

--- lab4/ConsoleApp1/ConsoleApp1/Program.cs
+++ lab4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp1
 
         public Angle(int degrees, int minutes)
         {
-            // Переконуємося, що хвилини знаходяться в межах [0, 59]
+            
             if (minutes >= 60 || minutes < 0)
             {
                 Console.WriteLine("Minutes must be between 0 and 59.");
@@ -23,13 +23,13 @@ namespace ConsoleApp1
             NormalizeAngle();
         }
 
-        // Переведення кута в радіани
+        
         public double ToRadians()
         {
             return (Degrees + Minutes / 60.0) * (Math.PI / 180.0);
         }
 
-        // Приведення кута до діапазону 0-360 (градації в 360-ті частки)
+        
         public void NormalizeAngle()
         {
             while (Degrees < 0)
@@ -52,7 +52,7 @@ namespace ConsoleApp1
             }
         }
 
-        // Операція збільшення кута на задану величину
+        
         public void Increase(int degreesToAdd, int minutesToAdd)
         {
             Degrees += degreesToAdd;
@@ -60,7 +60,7 @@ namespace ConsoleApp1
             NormalizeAngle();
         }
 
-        // Операція зменшення кута на задану величину
+        
         public void Decrease(int degreesToSubtract, int minutesToSubtract)
         {
             Degrees -= degreesToSubtract;
@@ -68,7 +68,7 @@ namespace ConsoleApp1
             NormalizeAngle();
         }
 
-        // Обчислення синуса кута
+        
         public double Sin()
         {
             return Math.Sin(ToRadians());
@@ -80,7 +80,7 @@ namespace ConsoleApp1
             return this.Degrees == other.Degrees && this.Minutes == other.Minutes;
         }
 
-        // Для зручності виведення
+       
         public override string ToString()
         {
             return $"{Degrees}° {Minutes}'";
@@ -93,25 +93,20 @@ namespace ConsoleApp1
         {
             try
             {
-                // Створення кута
+                
                 Angle angle = new Angle(45, 30);
                 Console.WriteLine($"Кут: {angle}");
 
-                // Переведення в радіани
                 Console.WriteLine($"Кут в радіанах: {angle.ToRadians()}");
-
-                // Збільшення кута
+                
                 angle.Increase(10, 15);
                 Console.WriteLine($"Після збільшення: {angle}");
 
-                // Зменшення кута
                 angle.Decrease(5, 45);
                 Console.WriteLine($"Після зменшення: {angle}");
 
-                // Обчислення синуса кута
                 Console.WriteLine($"Синус кута: {angle.Sin()}");
 
-                // Порівняння двох кутів без використання операторів
                 Angle angle2 = new Angle(49, 0);
                 Console.WriteLine($"Кути рівні? {angle.EqualsTo(angle2)}");
             }
