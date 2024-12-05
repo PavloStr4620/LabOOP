4b3f1c5d446198adab3c7d7387aa3f15dfb5b29f
Pavlo <112697824+PavloStr4620@users.noreply.github.com>
2024-12-05 22:33:11 +0200

    Update Program.cs

--- lab3/ConsoleApp1/ConsoleApp1/Program.cs
+++ lab3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,24 +6,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // Введення розміру масиву
+            
             Console.Write("Введіть кількість елементів масиву (n): ");
             int n = int.Parse(Console.ReadLine());
 
-            // Оголошення та ініціалізація масиву випадковими числами
             double[] array = new double[n];
             Random rnd = new Random();
 
             for (int i = 0; i < n; i++)
             {
-                // Генерація випадкових чисел у діапазоні [-14.2, 18.3] з точністю до 1 дробового знака
                 array[i] = Math.Round(rnd.NextDouble() * (18.3 - (-14.2)) + (-14.2), 1);
             }
 
             Console.WriteLine("Початковий масив: ");
             PrintArray(array);
 
-            // Знаходимо індекси мінімального та максимального елементів
             int minIndex = 0, maxIndex = 0;
             for (int i = 1; i < n; i++)
             {
@@ -34,7 +31,6 @@ namespace ConsoleApp1
             Console.WriteLine($"\nМінімальний елемент: {array[minIndex]} (індекс {minIndex})");
             Console.WriteLine($"Максимальний елемент: {array[maxIndex]} (індекс {maxIndex})");
 
-            // Гарантуємо, що minIndex менший за maxIndex
             if (minIndex > maxIndex)
             {
                 int temp = minIndex;
@@ -42,7 +38,6 @@ namespace ConsoleApp1
                 maxIndex = temp;
             }
 
-            // Завдання 1: Знайти суму індексів елементів між мінімальним і максимальним
             int sumIndexes = 0;
             for (int i = minIndex + 1; i < maxIndex; i++)
             {
@@ -50,14 +45,12 @@ namespace ConsoleApp1
             }
             Console.WriteLine($"\nСума індексів між мінімальним та максимальним елементами: {sumIndexes}");
 
-            // Завдання 2: Змінити порядок елементів між мінімальним та максимальним на протилежний
             ReverseSubArray(array, minIndex + 1, maxIndex - 1);
 
             Console.WriteLine("\nМасив після зміни порядку елементів між мінімальним та максимальним: ");
             PrintArray(array);
         }
 
-        // Функція для виведення масиву
         static void PrintArray(double[] array)
         {
             foreach (double num in array)
@@ -67,7 +60,6 @@ namespace ConsoleApp1
             Console.WriteLine();
         }
 
-        // Функція для реверсу частини масиву
         static void ReverseSubArray(double[] array, int start, int end)
         {
             while (start < end)
